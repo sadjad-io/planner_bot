4eded0feb93e08b06ec9c583295ed6e3425d5d78
Dr web <p30light.ir@gmail.com>
2017-06-24 20:07:56 +0000

    Ezafe Shodan Bakhsh Haye Decorator va takmli sakhtare Database be joz forgien Key

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import telebot
+from telebot import types
 import config
 from model import *
 import logging
@@ -9,12 +12,54 @@ import sys
 bot = telebot.TeleBot(config.token)
 session = scoped_session(sessionmaker(bind=engine))
 
+#Send Inline Message After Enter Bot Id ( @bot_id ... )
+@bot.inline_handler(lambda query: len(query.query) is 0)
+def first_query(inline_query):
+    try:
+        bot.answer_inline_query(inline_query.id, [], cache_time=1, switch_pm_parameter="123",switch_pm_text="ساخت برنامه جدید")
+    except Exception, e:
+        print(e)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, config.messages["welcome"])
+#Welcome Message After Enter Start Command
+@bot.message_handler(commands=['start'])
+@user
+def welcome(message):
+    try:
+        chat_id = message.chat.id
+        bot.send_message( chat_id , config.messages['welcome'] )
+        get_name(message)
+    except Exception, e:
+        print(e)
 
-
+@command
+def get_name(message):
+    try:
+        chat_id = message.chat.id
+        msg = bot.send_message( chat_id , config.messages['req_name'] )
+        bot.register_next_step_handler(msg , get_details )
+    except Exception, e:
+        print(e)
+        
+@command
+def get_details(message): 
+    try:
+        chat_id = message.chat.id
+        msg = bot.send_message( chat_id , config.messages['req_details'] )
+        bot.register_next_step_handler(msg , query_master )
+    except Exception, e:
+        print(e)
+        
+@command
+def query_master(message):
+    try:
+        chat_id = message.chat.id
+        keyboard = types.InlineKeyboardMarkup()
+        k1 = types.InlineKeyboardButton( "text" , switch_inline_query='text'  )
+        keyboard.add(k1)
+        bot.send_message(message.chat.id , "@EZPlanneBot test" , reply_markup=keyboard )
+    except Exception, e:
+        print(e)
+    
 if __name__ == '__main__':
     while True:
         try:
--- model.py
+++ model.py
@@ -3,13 +3,59 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+Session = sessionmaker()
 
-class Chat(Base):
-    __tablename__ = 'chats'
+class Users(Base):
+    __tablename__ = 'users'
 
     id = Column(Integer , primary_key=True)
-    chat_id = Column(Integer)
-    activate = Column(Boolean)
+    user_id = Column(Integer)
+    fname = Column(String(128))
+    lname = Column(String(128))
+    username = Column(String(128))
+    user_lvl = Column(Integer)
+    
+class Events(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer , primary_key=True)
+    owner_id = Column(Integer)
+    title = Column(String(128))
+    description = Column(String(1024))
+    agree_users = Column(String)
 
 engine = create_engine('sqlite:///planner.db')
 Base.metadata.create_all(engine)
+Session.configure(bind=engine)
+
+session = Session()
+
+'''
+Decorator : Check User States
+'''
+def user(f):
+    def check_user(message):
+        result = session.query(Users).filter_by(user_id=message.chat.id).first()
+        if result:
+            return True
+        else:
+            user = Users(user_id = message.chat.id , fname = message.chat.first_name , lname = message.chat.last_name , username = message.chat.username , user_lvl = 0)
+            session.add(user)
+            session.commit()
+            return True
+    def decorator(message):
+            check_user(message)
+            f(message)
+    return decorator
+    
+def command(f):
+    def check_lvl(message):
+        result = session.query(Users).filter_by(user_id=message.chat.id).first()
+        if result:
+            return result.user_lvl
+        else:
+            return False
+    def decorator(message):
+            check_lvl(message)
+            f(message)
+    return decorator
