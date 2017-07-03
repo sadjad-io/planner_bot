aa4c505482aa076b3e288f0a9a3a08822be4c7d1
root <root@ns1.parsdesign.site>
2017-07-03 03:24:40 -0400

    Final Edit

--- bot.py
+++ bot.py
@@ -12,11 +12,27 @@ import sys
 bot = telebot.TeleBot(config.token)
 session = scoped_session(sessionmaker(bind=engine))
 
+class Event_Det :
+    def __ini__(self):
+        self.title = None
+
 #Send Inline Message After Enter Bot Id ( @bot_id ... )
 @bot.inline_handler(lambda query: len(query.query) is 0)
 def first_query(inline_query):
     try:
-        bot.answer_inline_query(inline_query.id, [], cache_time=1, switch_pm_parameter="123",switch_pm_text="ساخت برنامه جدید")
+        events = session.query(Events).filter_by(owner_id=int(inline_query.from_user.id)).all()
+        session.commit()
+        event_list = []
+        counter = 1
+        for event in events:
+            keyboard = types.InlineKeyboardMarkup()
+            plan_text = event.text
+            k1 = types.InlineKeyboardButton( "من هستم" , callback_data="add_me" + "|" + str(event.id) )
+            k2 = types.InlineKeyboardButton( "من نیستم" , callback_data="remove_me" + "|" + str(event.id) )
+            keyboard.add(k1,k2)
+            event_list.append( types.InlineQueryResultArticle( str(counter) , u'انتشار پلن : ' + event.title + "( " + str( event.id ) + " )" , types.InputTextMessageContent( plan_text ) , reply_markup=keyboard ))
+            counter+=1
+        bot.answer_inline_query(inline_query.id, event_list , cache_time=1 , switch_pm_parameter="Create" ,switch_pm_text="ساخت برنامه جدید")
     except Exception, e:
         print(e)
 
@@ -31,41 +47,99 @@ def welcome(message):
     except Exception, e:
         print(e)
 
-@command
 def get_name(message):
     try:
         chat_id = message.chat.id
         msg = bot.send_message( chat_id , config.messages['req_name'] )
-        bot.register_next_step_handler(msg , get_details )
+        bot.register_next_step_handler( msg , get_details )
     except Exception, e:
         print(e)
-        
+
 @command
-def get_details(message): 
+def get_details(message):
     try:
         chat_id = message.chat.id
+        Event_Det.title = message.text
         msg = bot.send_message( chat_id , config.messages['req_details'] )
         bot.register_next_step_handler(msg , query_master )
     except Exception, e:
         print(e)
-        
+
 @command
 def query_master(message):
     try:
         chat_id = message.chat.id
+        event = Events(owner_id = chat_id , title = Event_Det.title , description = message.text , text = config.messages['plan'].format( message.chat.first_name , message.chat.username , event.title , event.description , " " , " " ) , agree_users = u" ", disagree_users = u" ")
+        session.add( event )
+        session.commit()
         keyboard = types.InlineKeyboardMarkup()
-        k1 = types.InlineKeyboardButton( "text" , switch_inline_query='text'  )
+        k1 = types.InlineKeyboardButton( "منتشر کردن در ..." , switch_inline_query=event.title )
         keyboard.add(k1)
-        bot.send_message(message.chat.id , "@EZPlanneBot test" , reply_markup=keyboard )
+        bot.send_message(message.chat.id , u"خوب \n برنامه ریزی مون در مورد" + event.title + u" ساخته شد " , reply_markup = keyboard)
     except Exception, e:
         print(e)
-    
+
+@bot.inline_handler(lambda query: len(query.query) > 0)
+def first_query(inline_query):
+    try:
+        query = inline_query.query
+        events = session.query(Events).filter(Events.title.contains(query)).all()
+        session.commit()
+        event_list = []
+        counter = 1
+        for event in events:
+            keyboard = types.InlineKeyboardMarkup()
+            plan_text = event.text
+            k1 = types.InlineKeyboardButton( "من هستم" , callback_data="add_me" + "|" + str(event.id) )
+            k2 = types.InlineKeyboardButton( "من نیستم" , callback_data="remove_me" + "|" + str(event.id) )
+            keyboard.add(k1,k2)
+            event_list.append( types.InlineQueryResultArticle( str(counter) , u'انتشار پلن : ' + event.title + "( " + str( event.id ) + " )" , types.InputTextMessageContent( plan_text ) , reply_markup=keyboard ))
+            counter+=1
+        bot.answer_inline_query(inline_query.id, event_list , cache_time=1 , switch_pm_parameter="Create" ,switch_pm_text="ساخت برنامه جدید")
+    except Exception, e:
+        print(e)
+
+@bot.callback_query_handler(func=lambda call : True )
+def callback_inline(call):
+    data = call.data.split( '|' )
+    if data[0]=="add_me": ##Add note
+        event = session.query(Events).filter_by(id=int(data[1])).first()
+        session.commit()
+        owner = session.query(Users).filter_by(user_id=int(event.owner_id)).first()
+        session.commit()
+        user = config.messages['user'].format( call.from_user.first_name , call.from_user.last_name , call.from_user.username )
+        if user in event.disagree_users:
+            event.disagree_users = event.disagree_users.replace( user , "")
+        if user in event.agree_users:
+            event.agree_users = event.agree_users.replace( user , "")
+            bot.answer_callback_query( callback_query_id= call.id , text = u"اسمت از تو لیست اونایی که میان حذف شد" , cache_time = 0 )
+        else:
+            event.agree_users+= user
+        keyboard = types.InlineKeyboardMarkup()
+        k1 = types.InlineKeyboardButton( "من هستم" , callback_data="add_me" + "|" + str(event.id) )
+        k2 = types.InlineKeyboardButton( "من نیستم" , callback_data="remove_me" + "|" + str(event.id) )
+        keyboard.add(k1,k2)
+        bot.edit_message_text(inline_message_id = call.inline_message_id , text = config.messages['plan'].format( owner.fname , owner.username , event.title , event.description , event.agree_users , event.disagree_users ) , reply_markup = keyboard )
+        bot.answer_callback_query( callback_query_id= call.id , text = u"اسمت با موفقیت به لیست اضافه شد 😍" , cache_time = 0 )
+
+    if data[0]=="remove_me": ##Add note
+        event = session.query(Events).filter_by(id=int(data[1])).first()
+        session.commit()
+        owner = session.query(Users).filter_by(user_id=int(event.owner_id)).first()
+        session.commit()
+        user = config.messages['user'].format( call.from_user.first_name , call.from_user.last_name , call.from_user.username )
+        if user in event.agree_users:
+            event.agree_users = event.agree_users.replace( user , "")
+        if user in event.disagree_users:
+            event.disagree_users = event.disagree_users.replace( user , "")
+            bot.answer_callback_query( callback_query_id= call.id , text = u"اسمت از تو لیست اونایی که نمیان حذف شد" , cache_time = 0 )
+        else:
+            event.disagree_users+= user
+        keyboard = types.InlineKeyboardMarkup()
+        k1 = types.InlineKeyboardButton( "من هستم" , callback_data="add_me" + "|" + str(event.id) )
+        k2 = types.InlineKeyboardButton( "من نیستم" , callback_data="remove_me" + "|" + str(event.id) )
+        keyboard.add(k1,k2)
+        bot.edit_message_text(inline_message_id = call.inline_message_id , text = config.messages['plan'].format( owner.fname , owner.username , event.title , event.description , event.agree_users , event.disagree_users ) , reply_markup = keyboard )
+        bot.answer_callback_query( callback_query_id= call.id , text = u"اسمت با موفقیت به لیست اضافه شد 😍" , cache_time = 0 )
 if __name__ == '__main__':
-    while True:
-        try:
-            bot.polling(none_stop=True)
-        except KeyboardInterrupt:
-            sys.exit()
-        except Exception as err:
-            logging.error(err)
-            sleep(5)
+    bot.polling(none_stop=True)
--- model.py
+++ model.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import *
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
+import config
 
 Base = declarative_base()
-Session = sessionmaker()
+engine = create_engine('sqlite:///planner.db?check_same_thread=False')
 
 class Users(Base):
     __tablename__ = 'users'
@@ -13,49 +18,63 @@ class Users(Base):
     fname = Column(String(128))
     lname = Column(String(128))
     username = Column(String(128))
-    user_lvl = Column(Integer)
-    
+    user_lvl = Column(String)
+
 class Events(Base):
     __tablename__ = 'events'
 
     id = Column(Integer , primary_key=True)
-    owner_id = Column(Integer)
-    title = Column(String(128))
-    description = Column(String(1024))
-    agree_users = Column(String)
+    owner_id = Column(String(128))
+    title = Column( String(128) , server_default= u"وارد نشده" )
+    description = Column(String(1024) , server_default= u"وارد نشده")
+    text = Column(Text , server_default= u"وارد نشده")
+    agree_users = Column(Text)
+    disagree_users = Column(Text)
 
-engine = create_engine('sqlite:///planner.db')
 Base.metadata.create_all(engine)
-Session.configure(bind=engine)
-
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
 session = Session()
 
 '''
 Decorator : Check User States
 '''
-def user(f):
+def user(function):
     def check_user(message):
         result = session.query(Users).filter_by(user_id=message.chat.id).first()
         if result:
             return True
         else:
-            user = Users(user_id = message.chat.id , fname = message.chat.first_name , lname = message.chat.last_name , username = message.chat.username , user_lvl = 0)
+            user = Users(user_id = message.chat.id , fname = message.chat.first_name , lname = message.chat.last_name , username = message.chat.username , user_lvl = "start")
             session.add(user)
             session.commit()
             return True
     def decorator(message):
             check_user(message)
-            f(message)
+            function(message)
     return decorator
-    
-def command(f):
-    def check_lvl(message):
-        result = session.query(Users).filter_by(user_id=message.chat.id).first()
-        if result:
-            return result.user_lvl
+
+def command(function):
+    def check_message(message):
+        if( message.text in config.commands ):
+            return True
         else:
             return False
+
+    def check_lvl(message):
+        if( message.text in config.commands ):
+            return True
+        else:
+            return False
+
+    def set_lvl(message,f_name):
+        user = session.query(Users).filter_by(user_id=message.chat.id).first()
+        user.user_lvl = f_name.__name__
+        session.commit()
     def decorator(message):
-            check_lvl(message)
-            f(message)
+            set_lvl(message,function)
+            if( check_lvl(message) ):
+                pass
+            else:
+                function(message)
     return decorator
